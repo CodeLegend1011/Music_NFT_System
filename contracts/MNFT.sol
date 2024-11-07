07db92567841ebdf650366f2fdf65803b7acc26b
CodeLegend1011 <142136719+CodeLegend1011@users.noreply.github.com>
2024-11-07 22:07:22 +0530

    MNFT.sol

--- contracts/MNFT.sol
+++ contracts/MNFT.sol
@@ -11,12 +11,16 @@ contract MusicNFT is ERC721 {
         string tokenURI;       // IPFS URI for metadata
         address artist;        // Address of the artist
         uint256 royaltyPercentage; // Royalty percentage for the artist
+        uint256 price;         // Price of the NFT
     }
 
     Counters.Counter private _tokenIdCounter; // Counter for token IDs
     mapping(uint256 => NFT) public nfts; // Mapping of token ID to NFT details
+    mapping(address => uint256) public artistRoyalties; // Mapping of artist address to total royalties earned
+    mapping(uint256 => bool) public nftSold; // Mapping to track if an NFT has been sold
 
-    event MusicNFTMinted(uint256 tokenId, address artist, string tokenURI, uint256 royaltyPercentage);
+    event MusicNFTMinted(uint256 tokenId, address artist, string tokenURI, uint256 royaltyPercentage, uint256 price);
+    event NFTPurchased(address buyer, uint256 tokenId, uint256 price);
 
     constructor() ERC721("MusicNFT", "MNFT") {}
 
@@ -24,8 +28,9 @@ contract MusicNFT is ERC721 {
      * @dev Upload a new music NFT
      * @param _tokenURI The URI for the NFT's metadata
      * @param _royaltyPercentage The royalty percentage for the artist
+     * @param _price The price of the NFT
      */
-    function uploadMusicNFT(string memory _tokenURI, uint256 _royaltyPercentage) external {
+    function uploadMusicNFT(string memory _tokenURI, uint256 _royaltyPercentage, uint256 _price) external {
         require(_royaltyPercentage <= 100, "Royalty percentage must be between 0 and 100");
 
         uint256 tokenId = _tokenIdCounter.current();
@@ -35,12 +40,70 @@ contract MusicNFT is ERC721 {
         nfts[tokenId] = NFT({
             tokenURI: _tokenURI,
             artist: msg.sender,
-            royaltyPercentage: _royaltyPercentage
+            royaltyPercentage: _royaltyPercentage,
+            price: _price
         });
 
         _tokenIdCounter.increment(); // Increment the token ID counter
 
-        emit MusicNFTMinted(tokenId, msg.sender, _tokenURI, _royaltyPercentage);
+        emit MusicNFTMinted(tokenId, msg.sender, _tokenURI, _royaltyPercentage, _price);
+    }
+
+    /**
+     * @dev Function to purchase an NFT
+     * @param tokenId The token ID of the NFT being purchased
+     */
+    function purchaseNFT(uint256 tokenId) external payable {
+        require(msg.value == nfts[tokenId].price, "Incorrect value sent");
+        require(!nftSold[tokenId], "NFT already sold");
+
+        // Transfer the payment to the artist
+        address artist = nfts[tokenId].artist;
+        uint256 royaltyAmount = (msg.value * nfts[tokenId].royaltyPercentage) / 100;
+        uint256 artistPayment = msg.value - royaltyAmount;
+
+        // Pay the artist and record the royalty
+        payable(artist).transfer(artistPayment);
+        artistRoyalties[artist] += royaltyAmount;
+
+        // Mark the NFT as sold
+        nftSold[tokenId] = true;
+
+        // Transfer the ownership of the NFT
+        _transfer(artist, msg.sender, tokenId);
+
+        emit NFTPurchased(msg.sender, tokenId, msg.value);
+    }
+
+    /**
+     * @dev Get the total number of NFTs sold
+     * @return uint256 The total number of NFTs sold
+     */
+    function getNFTsSold() public view returns (uint256) {
+        uint256 soldCount = 0;
+        for (uint256 i = 0; i < _tokenIdCounter.current(); i++) {
+            if (nftSold[i]) {
+                soldCount++;
+            }
+        }
+        return soldCount;
+    }
+
+    /**
+     * @dev Get the total royalties earned by an artist
+     * @param artist The address of the artist
+     * @return uint256 The total royalties earned by the artist
+     */
+    function getArtistRoyalties(address artist) public view returns (uint256) {
+        return artistRoyalties[artist];
+    }
+
+    /**
+     * @dev Get the current token ID counter value
+     * @return uint256 The current token ID counter value
+     */
+    function getTokenIdCounter() public view returns (uint256) {
+        return _tokenIdCounter.current();
     }
 
     /**
